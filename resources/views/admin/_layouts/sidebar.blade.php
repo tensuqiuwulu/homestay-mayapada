642486e98fb796ac2759aa232d7743e72c83a340
Tensu Qiuwulu <tensuqiuwulu@192.168.1.2>
2024-01-04 20:07:55 +0800

    update

--- resources/views/admin/_layouts/sidebar.blade.php
+++ resources/views/admin/_layouts/sidebar.blade.php
@@ -8,28 +8,28 @@
   </div>
   <!--navigation-->
   <ul class="metismenu" id="menu">
-    <li class="">
+    <li class="{{ Request::routeIs(['admin.images']) ? 'mm-active' : '' }}">
       <a href="{{ route('admin.images') }}">
         <div class="parent-icon"><i class="bx bx-images"></i>
         </div>
-        <div class="menu-title">Gambar Villa</div>
+        <div class="menu-title">Gambar Homestay</div>
       </a>
     </li>
-    <li class="">
+    <li class="{{ Request::routeIs(['admin.room']) ? 'mm-active' : '' }}">
       <a href="{{ route('admin.room') }}">
         <div class="parent-icon"><i class="bx bx-home-smile"></i>
         </div>
         <div class="menu-title">Manage Rooms</div>
       </a>
     </li>
-    <li class="">
+    <li class="{{ Request::routeIs(['admin.booking']) ? 'mm-active' : '' }}">
       <a href="{{ route('admin.booking') }}">
         <div class="parent-icon"><i class="bx bx-shopping-bag"></i>
         </div>
         <div class="menu-title">Manage Booking</div>
       </a>
     </li>
-    <li class="">
+    <li class="{{ Request::routeIs(['admin.contact']) ? 'mm-active' : '' }}">
       <a href="{{ route('admin.contact') }}">
         <div class="parent-icon"><i class="bx bx-phone"></i>
         </div>
